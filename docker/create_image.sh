ee5a647650d3b1419f0c5e8ab31a5d6885163397
etentor <e.tentor.2021@alumnos.urjc.es>
2025-06-21 22:09:15 +0200

    Docker update

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# 1. Construye la imagen Docker de tu app
+# 1. Construir la imagen Docker
 docker build -t etentor2021/arteum:latest .
 
-# 2. Subir la imagen a Docker Hub
+# 2. Subir la imagen a Docker
 docker push etentor2021/arteum:latest
 
 # 3. Levantar la app y la base de datos usando Docker Compose
